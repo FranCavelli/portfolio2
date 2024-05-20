bb0611660c6080a0316f2d7259fc71fe9f86187e
FranCavelli <FranCavelli@hotmail.com>
2024-05-20 10:03:06 -0300

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import threePlugin from 'vite-plugin-three';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://FranCavelli.github.io',
   base: '/portfolio2/',
-  integrations: [tailwind(), react(), threePlugin()]
+  vite: {
+    plugins: [threePlugin()],
+  },
+  integrations: [tailwind(), react()]
 });
